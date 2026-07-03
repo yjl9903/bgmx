d2dfd940a0e59b8ac320860dad1c0aa19cc0a13c
KazariEX <zinkawaii@qq.com>
2026-07-03 11:52:17 +0800

    chore: ship esm only

--- apps/bgmw/tsdown.config.ts
+++ apps/bgmw/tsdown.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: {
-    index: 'src/index.ts'
-  },
-  format: 'esm',
-  dts: true,
-  clean: true,
-  outDir: 'dist',
-  target: 'esnext'
+  exports: true,
 });
