22370f96f0138a761631f60f908d78e22ad210db
yjl9903 <yjl9903@onekuma.cn>
2026-07-04 21:46:39 +0800

    chore: export bgmx/client

--- apps/bgmx/tsdown.config.ts
+++ apps/bgmx/tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
+    client: 'src/client/index.ts',
     cli: 'src/cli.ts'
   },
   exports: true,
